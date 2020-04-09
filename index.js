6a5f23888f86d732b3f76d5cba535208390f9eca
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-08 19:35:15 -0700

    create write request

---
+++ index.js
@@ -0,0 +1,51 @@
+const request = require('request');
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const [url, filePath] = process.argv.slice(2);
+
+const writeToFile = function(body, filePath) {
+  fs.writeFile(filePath, body, function (err) {
+    if (err) throw err;
+    fs.stat(filePath, function(err, stats) {
+      if (err) throw err;
+      console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
+      rl.close()
+    })
+  });
+}
+
+const writeToExistingFile = function(body, filePath) {
+  rl.question('File path exists. overwrite? [Y/N] >> ', function(answer) {
+    if (answer.toUpperCase() === 'Y') {
+      writeToFile(body, filePath);
+    } else {
+      rl.close()
+    }
+  })
+}
+
+const writeRequest = function(url, filePath) {
+  request(url, (err, response, body) => {
+    if (err) throw err;
+    if (response.statusCode !== 200) {
+      console.log(`Status Code: ${response.statusCode}`);
+      rl.close()
+    } else {
+      fs.exists(filePath, function(exists) {
+        if (exists) {
+          writeToExistingFile(body, filePath)
+        } else {
+          writeToFile(body, filePath);
+        }
+      });
+    }
+  });
+}
+
+writeRequest(url, filePath);
