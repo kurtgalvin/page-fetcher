e71a2800e9539385087196a0db9340e2fc914e18
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-04-08 20:36:42 -0700

    refactor readline in writeToExistingFile

--- index.js
+++ index.js
@@ -2,26 +2,26 @@ const request = require('request');
 const fs = require('fs');
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-const writeToFile = function(body, filePath) {
+const writeToFile = function(body, filePath, rl = null) {
   fs.writeFile(filePath, body, function (err) {
     if (err) throw err;
     fs.stat(filePath, function(err, stats) {
       if (err) throw err;
       console.log(`Downloaded and saved ${stats.size} bytes to ${filePath}`);
-      rl.close()
+      if (rl) rl.close()
     })
   });
 }
 
 const writeToExistingFile = function(body, filePath) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
   rl.question('File path exists. overwrite? [Y/N] >> ', function(answer) {
     if (answer.toUpperCase() === 'Y') {
-      writeToFile(body, filePath);
+      writeToFile(body, filePath, rl);
     } else {
       rl.close()
     }
@@ -33,7 +33,6 @@ const writeRequest = function(url, filePath) {
     if (err) throw err;
     if (response.statusCode !== 200) {
       console.log(`Status Code: ${response.statusCode}`);
-      rl.close()
     } else {
       fs.exists(filePath, function(exists) {
         if (exists) {
